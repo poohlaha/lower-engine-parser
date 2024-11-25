d89566a7f8bc5fcc85564319c190d0fab7932a4b
poohlaha <smile.love.tao@gmil.com>
2024-11-25 17:22:49 +0800

    update():  添加对话框属性

--- src/lib/setters/iconDialog/index.tsx
+++ src/lib/setters/iconDialog/index.tsx
@@ -6,6 +6,8 @@
 import React, { ReactElement, useEffect, useState } from 'react'
 import { ICommonProps } from '../../utils/common'
 import { Modal, Tabs } from 'antd'
+import InputSetter from '../input'
+import Utils from '../../utils/utils'
 
 export interface IIconDialogProps extends ICommonProps {
   zIndex?: number
@@ -31,6 +33,7 @@ export interface IIconDialogTabProps {
 
 const MIconDialog = (props: IIconDialogProps): ReactElement => {
   const [tabActiveKey, setTabActiveKey] = useState('0')
+  const [searchValue, setSearchValue] = useState('')
   const [open, setOpen] = useState(false)
   const [selected, setSelected] = useState<{ [K: string]: any }>({})
 
@@ -52,16 +55,26 @@ const MIconDialog = (props: IIconDialogProps): ReactElement => {
     return items
   }
 
+  // 查找图标
+  const getSearchValues = (iconObj: IIconDialogTabProps = {}) => {
+    const iconValues = iconObj.values || []
+    if (Utils.isBlank(searchValue)) {
+      return iconValues || []
+    }
+
+    return iconValues.filter((icon: { [K: string]: any }) => (icon.text || '').indexOf(searchValue) !== -1)
+  }
+
   const render = () => {
     const iconObj: IIconDialogTabProps = (props.tabs || []).find((tab, index: number) => `${index}` === tabActiveKey) || {}
-    const iconValues = iconObj.values || []
+    const iconValues = getSearchValues(iconObj || {})
     return (
       <div className={`${props.className || ''} lower-engine-icon-dialog`}>
         <button onClick={() => setOpen(true)}>{props.icon ? props.icon : props.text || ''}</button>
 
         <Modal
           wrapClassName={`lower-engine-icon-dialog-modal ${props.dialogClassName || ''}`}
-          title={props.title || '对话框'}
+          title=""
           maskClosable={props.maskClosable}
           width={props.width}
           style={props.dialogStyle}
@@ -81,9 +94,24 @@ const MIconDialog = (props: IIconDialogProps): ReactElement => {
           afterOpenChange={(newOpen: boolean) => {
             setOpen(newOpen)
             props.onOpenChange?.(newOpen)
+            if (!newOpen) {
+              setSearchValue('')
+            }
           }}
         >
           <div className="lower-engine-icon-dialog-modal-content flex-direction-column">
+            <div className="icon-dialog-modal-content-header flex-align-center">
+              <div className="header-title">{props.title || '对话框'}</div>
+              <InputSetter
+                className="flex-1"
+                default={setSearchValue}
+                isSearch={true}
+                onChange={(value: string = '') => {
+                  setSearchValue(value)
+                }}
+              />
+            </div>
+
             <Tabs
               items={getTabItems()}
               activeKey={tabActiveKey}
@@ -97,6 +125,22 @@ const MIconDialog = (props: IIconDialogProps): ReactElement => {
               <div className="lower-engine-icon-dialog-modal-content-tab">
                 {iconValues.length > 0 &&
                   iconValues.map((value: { [K: string]: any }, index: number) => {
+                    let text = value.text || ''
+                    let hasDanger = false
+                    let content: string | React.ReactNode = value.text || ''
+                    if (!Utils.isBlank(searchValue || '')) {
+                      let searchIndex = text.indexOf(searchValue || '')
+                      if (searchIndex !== -1) {
+                        content = (
+                          <div className="text-c search-text flex-align-center">
+                            <p>{text.substring(0, searchIndex)}</p>
+                            <p className="search-value">{searchValue}</p>
+                            <p>{text.substring(searchIndex + searchValue.length, text.length)}</p>
+                          </div>
+                        )
+                        hasDanger = true
+                      }
+                    }
                     return (
                       <div
                         className={`tab-icon flex-center flex-direction-column ${selected.index === index ? 'active' : ''}`}
@@ -109,7 +153,8 @@ const MIconDialog = (props: IIconDialogProps): ReactElement => {
                         }}
                       >
                         {value.component}
-                        <p className="text-c">{value.text || ''}</p>
+
+                        {!hasDanger ? <p className="text-c">{content || ''}</p> : content}
                       </div>
                     )
                   })}
--- src/lib/setters/input/index.tsx
+++ src/lib/setters/input/index.tsx
@@ -19,6 +19,7 @@ export interface IInputProps extends ICommonProps {
   suffix?: React.ReactNode
   type?: string
   readOnly?: boolean
+  isSearch?: boolean
   onChange?: (value: string) => void
 }
 
@@ -38,18 +39,31 @@ const Input = (props: IInputProps): ReactElement => {
     setValue(defaultValue)
   }, [props.default])
 
+  const getSearchSvg = () => {
+    const isSearch = props.isSearch ?? false
+    if (!isSearch) return props.prefix
+    return (
+      <svg viewBox="64 64 896 896" focusable="false" data-icon="search" fill="currentColor" aria-hidden="true">
+        <path
+          fill="currentColor"
+          d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"
+        ></path>
+      </svg>
+    )
+  }
+
   const render = () => {
     const alignmentClassName = Utils.getComponentAlignmentClassName(props.title || '', props.alignment || '')
     return (
       <div className={`${props.className || ''} lower-engine-input ${alignmentClassName || ''}`}>
-        <div className={`${alignmentClassName || ''}`}>
+        <div className={`${alignmentClassName || ''} lower-engine-input-wrapper flex-1`}>
           {!Utils.isBlank(props.text || '') && <MText text={props.text || ''} tooltip={props.tooltip || ''} textClassName="over-ellipsis" />}
           <AntdInput
             placeholder={props.placeholder || '请输入'}
             allowClear={props.allowClear ?? true}
             disabled={props.disabled ?? false}
             maxLength={props.maxLength}
-            prefix={props.prefix}
+            prefix={getSearchSvg()}
             showCount={props.showCount}
             suffix={props.suffix}
             type={props.type}
