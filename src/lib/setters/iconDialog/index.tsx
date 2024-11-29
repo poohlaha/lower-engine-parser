3e0592d97ec5daa8f28693cbb86b90e184dedd3e
poohlaha <smile.love.tao@gmil.com>
2024-11-29 17:00:02 +0800

    update(): 修改 SelectorSetter

--- src/lib/setters/iconDialog/index.tsx
+++ src/lib/setters/iconDialog/index.tsx
@@ -163,7 +163,7 @@ const MIconDialog = (props: IIconDialogProps): ReactElement => {
                           })
                         }}
                       >
-                        {value.component()}
+                        {value.component?.()}
 
                         {!hasDanger ? <p className="text-c">{content || ''}</p> : content}
                       </div>
